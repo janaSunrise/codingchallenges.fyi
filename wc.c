a106b286c97981d926a6ceb1ba68a3c0b74d656d
Sunrit Jana <warriordefenderz@gmail.com>
2026-05-03 00:07:06 +0530

    rename function. add multibyte character mode

--- wc.c
+++ wc.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <locale.h>
+#include <wchar.h>
 
-int get_char_count(FILE *fp) {
+int count_chars(FILE *fp) {
   int count = 0;
   while (fgetc(fp) != EOF) count++;
   return count;
 }
 
-int get_line_count(FILE *fp) {
+int count_lines(FILE *fp) {
   int c;
   int count = 0;
 
@@ -18,7 +20,7 @@ int get_line_count(FILE *fp) {
   return count;
 }
 
-int get_word_count(FILE *fp) {
+int count_words(FILE *fp) {
   int c;
   int count = 0;
   int in_word = 0;
@@ -35,7 +37,15 @@ int get_word_count(FILE *fp) {
   return count;
 }
 
+int count_multibyte_chars(FILE *fp) {
+  int count = 0;
+  while (fgetwc(fp) != WEOF) count++;
+  return count;
+}
+
 int main(int argc, char* argv[]) {
+  setlocale(LC_ALL, "");
+
   if (argc != 3) {
     printf("error: invalid number of arguments\n");
     printf("usage: ./mywc <mode> <filepath>\n");
@@ -57,15 +67,19 @@ int main(int argc, char* argv[]) {
 
   switch (mode) {
     case 'c':
-      printf("%d %s\n", get_char_count(file), filepath);
+      printf("%d %s\n", count_chars(file), filepath);
       break;
 
     case 'l':
-      printf("%d %s\n", get_line_count(file), filepath);
+      printf("%d %s\n", count_lines(file), filepath);
       break;
 
     case 'w':
-      printf("%d %s\n", get_word_count(file), filepath);
+      printf("%d %s\n", count_words(file), filepath);
+      break;
+
+    case 'm':
+      printf("%d %s\n", count_multibyte_chars(file), filepath);
       break;
 
     default:
