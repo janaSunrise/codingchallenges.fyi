8ac3cf7e165415b439c6a97a4c1ac141d9c8f19c
Sunrit Jana <warriordefenderz@gmail.com>
2026-05-05 00:14:07 +0530

    complete the wc tool

--- wc.c
+++ wc.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <locale.h>
-#include <wchar.h>
+#include <wchar.h> // wide character handling. `fgetwc`, similar to `fgetc`
 
 int count_chars(FILE *fp) {
   int count = 0;
@@ -43,52 +43,78 @@ int count_multibyte_chars(FILE *fp) {
   return count;
 }
 
+// single-pass counter so the default `wc <file>` doesn't need to rewind
+void count_all(FILE *fp, int *lines, int *words, int *chars) {
+  int c;
+  int in_word = 0;
+  *lines = *words = *chars = 0;
+
+  while ((c = fgetc(fp)) != EOF) {
+    (*chars)++;
+    if (c == '\n') (*lines)++;
+    if (isspace(c)) {
+      in_word = 0;
+    } else if (!in_word) {
+      in_word = 1;
+      (*words)++;
+    }
+  }
+}
+
 int main(int argc, char* argv[]) {
   setlocale(LC_ALL, "");
 
-  if (argc != 3) {
-    printf("error: invalid number of arguments\n");
-    printf("usage: ./mywc <mode> <filepath>\n");
-    return 1;
+  /* argument formats:
+   *   wc                -> default counts on stdin
+   *   wc <file>         -> default counts on file
+   *   wc -<m>           -> mode counts on stdin (e.g. `cmd | wc -c`)
+   *   wc -<m> <file>    -> mode counts on file */
+
+  char mode = 0;
+  const char *filepath = "";
+  FILE *file = NULL;
+
+  int argi = 1;
+  if (argi < argc && argv[argi][0] == '-') {
+    if (sscanf(argv[argi], "-%c", &mode) != 1) {
+      fprintf(stderr, "error: invalid mode flag. supported: -c, -l, -w, -m\n");
+      return 1;
+    }
+    argi++;
   }
 
-  char mode;
-  if (sscanf(argv[1], "-%c", &mode) != 1) {
-    printf("error: invalid mode flag. supported flags: -c, -l, -w\n");
+  if (argi < argc) {
+    filepath = argv[argi];
+    file = fopen(filepath, "r");
+    if (file == NULL) {
+      fprintf(stderr, "error: couldn't open file '%s'\n", filepath);
+      return 1;
+    }
+  } else {
+    file = stdin;
   }
 
-  char* filepath = argv[2];
-
-  FILE *file = fopen(filepath, "r");
-  if (file == NULL) {
-    printf("error: couldn't open file\n");
-    return 1;
+  int rc = 0;
+  if (mode == 0) {
+    int lines, words, chars;
+    count_all(file, &lines, &words, &chars);
+    printf("%d %d %d %s\n", lines, words, chars, filepath);
+  } else {
+    int n = 0;
+    int ok = 1;
+    switch (mode) {
+      case 'c': n = count_chars(file); break;
+      case 'l': n = count_lines(file); break;
+      case 'w': n = count_words(file); break;
+      case 'm': n = count_multibyte_chars(file); break;
+      default:
+        fprintf(stderr, "error: invalid mode flag '-%c'\n", mode);
+        ok = 0;
+        rc = 1;
+    }
+    if (ok) printf("%d %s\n", n, filepath);
   }
 
-  switch (mode) {
-    case 'c':
-      printf("%d %s\n", count_chars(file), filepath);
-      break;
-
-    case 'l':
-      printf("%d %s\n", count_lines(file), filepath);
-      break;
-
-    case 'w':
-      printf("%d %s\n", count_words(file), filepath);
-      break;
-
-    case 'm':
-      printf("%d %s\n", count_multibyte_chars(file), filepath);
-      break;
-
-    default:
-      printf("error: invalid mode flag\n");
-  }
-
-  // safely close file when done
-  fclose(file);
-
-  return 0;
+  if (file != stdin) fclose(file);
+  return rc;
 }
-
